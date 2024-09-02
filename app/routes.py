c9e37aa43c702425f3fe164ee9115f938e106dd3
brendanquirk <brendanquirkdev@gmail.com>
2024-09-02 13:23:47 -0400

    added template for posts

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-  return "Testing Route"
+  user = {'username': 'Brendan'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': 'First post!'
+    },
+    {
+      'author': {'username': 'Jane'},
+      'body': 'Hello, world!'
+    }
+  ]
+  
+  return render_template('index.html', title='Home', user=user, posts=posts)
