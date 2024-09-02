70c7a73aadfb3ff01ddcc228e3ba05d464966c49
brendanquirk <brendanquirkdev@gmail.com>
2024-09-02 14:18:31 -0400

    configured initial login form

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,26 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-  user = {'username': 'Brendan'}
-  posts = [
-    {
-      'author': {'username': 'John'},
-      'body': 'First post!'
-    },
-    {
-      'author': {'username': 'Jane'},
-      'body': 'Hello, world!'
-    }
-  ]
-  
-  return render_template('index.html', title='Home', user=user, posts=posts)
+    user = {'username': 'Brendan'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'First post'
+        },
+        {
+            'author': {'username': 'Jane'},
+            'body': 'Second post'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+from flask import render_template, request
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
